e27e9c1f79c4ca9cfaf2fede471975856f98fcd5
mcruzr85 <mcruzrodriguez1985@gmail.com>
2023-11-13 23:56:55 -0300

    creando contador de gastos x categorias

--- src/BusinessClasses/ContenedorDeCategorias.java
+++ src/BusinessClasses/ContenedorDeCategorias.java
@@ -1,10 +1,13 @@
 package BusinessClasses;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class ContenedorDeCategorias {
     List<Categoria> categoryList = new ArrayList<>();
+
 
     public ContenedorDeCategorias() {
 
--- src/BusinessClasses/ContenedorDeGastos.java
+++ src/BusinessClasses/ContenedorDeGastos.java
@@ -3,11 +3,15 @@ package BusinessClasses;
 import Interfaces.GastoOperations;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class ContenedorDeGastos implements GastoOperations {
 
     private List<GastoReal> gastosList = new ArrayList<>();
+    Map<String, Integer> contadorDeCategoriasPorNombre = new HashMap<>();
 
     public ContenedorDeGastos(){
 
@@ -28,7 +32,16 @@ public class ContenedorDeGastos implements GastoOperations {
 
     @Override
     public void addGasto(GastoReal gasto) {
+        String catName = gasto.getCategoria().getNombre();
+      Map<String, Integer> clavequebusco;
+        clavequebusco = contadorDeCategoriasPorNombre.stream()
+                .filter(elemento -> elemento.getKey() == catName)
+                .collect(Collectors.toMap(<String, Integer>));
+
         this.gastosList.add(gasto);
+
+
+        this.contadorDeCategoriasPorNombre.put( )
     }
 
     //para mostrar todos los gastos de la lista de gastos
